db795bbb23446cec43006b407dd21ed2437866b1
Luke Chen <luke.chen.2010@gmail.com>
2020-05-27 15:41:46 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_data_to_shows.rb
+++ db/migrate/007_add_data_to_shows.rb
@@ -1,7 +1,7 @@
 class AddDataToShows < ActiveRecord::Migration[5.1]
   
   def change
-    add_column :shows, :day, :string, :genre, :string, :season, :string
+    add_columns :shows, :day, :string, :genre, :string, :season, :string
   end
   
 end
